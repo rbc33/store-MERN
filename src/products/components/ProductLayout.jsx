ec069da01926458e4c35338c2ae38d55dcb03ccd
rbc33 <ricardobenthem@gmail.com>
2025-07-19 17:38:19 +0200

    is loadisg with conditionall useState

--- src/products/components/ProductLayout.jsx
+++ src/products/components/ProductLayout.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Loading from './loading'
 import AddButton from './addButton'
 
 const ProductLayout = () => {
+	const [isLoading, setIsLoading] = useState(true)
 	return (
 		<>
 			<Header title="Product app" />
 			<AddButton />
-			<Loading />
+			{isLoading ? <Loading /> : 'mostrar resultados'}
 		</>
 	)
 }
