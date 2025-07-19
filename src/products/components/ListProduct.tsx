609043483c214927e9b9e95285500e776ac9dcc2
rbc33 <ricardobenthem@gmail.com>
2025-07-19 23:13:14 +0200

    getting products with axios

--- src/products/components/ListProduct.tsx
+++ src/products/components/ListProduct.tsx
@@ -1,21 +1,39 @@
 import { Button } from 'react-bulma-components'
 import Loading from './Loading'
 import { useEffect, useState } from 'react'
+import axios, { Axios } from 'axios'
+import { BASE_URL } from '../../utils/config'
 
+type Product = {
+	nombre: string
+	descripcion: string
+	size: number
+	unitaryPrice: number
+	imgUrl: string
+}
+const getProducts = async () => {
+	axios({
+		url: `${BASE_URL}/products`,
+		method: 'GET',
+	})
+		.then((res) => {
+			return res
+		})
+		.catch((e) => console.log(e))
+}
 const ListProduct = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(true)
+	const [products, setProducts] = useState<Product[] | []>([])
 
 	useEffect(() => {
-		const timeId = setInterval(() => {
-			console.log('useEffect')
-			setIsLoading(!isLoading)
-		}, 2000)
-
-		return () => clearInterval(timeId)
+		const loadProducts = async () => {
+			const res = getProducts()
+			console.log(res)
+			return res
+		}
+		loadProducts()
 	})
-	useEffect(() => {
-		console.log('useEfectOnce')
-	}, [isLoading])
+
 	return <>{isLoading ? <Loading /> : 'di cosas'}</>
 }
 export default ListProduct
